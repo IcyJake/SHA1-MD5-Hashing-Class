23772d290fb0292ce013ee63447d40b6b2f51dc5
Jake <32722799+IcyJake@users.noreply.github.com>
2021-03-19 02:52:29 -0400

    Update source.cs

--- source.cs
+++ source.cs
@@ -1,22 +1,28 @@
 public class Hash
+{
+    public string SHA1(string input)
     {
-        public string SHA1(string input)
+        byte[] hash;
+
+        using (var sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider())
         {
-            byte[] hash;
-            using (var sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider())
-            {
-                hash = sha1.ComputeHash(Encoding.Unicode.GetBytes(input));
-            }
-            var sb = new StringBuilder();
-            foreach (byte b in hash) sb.AppendFormat("{0:x2}", b);
-            return sb.ToString();
+            hash = sha1.ComputeHash(Encoding.Unicode.GetBytes(input));
         }
-        public string MD5(string input)
-        {
-            MD5 md5 = new MD5CryptoServiceProvider();
-            string pass = Console.ReadLine();
-            byte[] checkSum = md5.ComputeHash(Encoding.Unicode.GetBytes(input));
-            string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
-            return result.ToLower();
-        }
+
+        var sb = new StringBuilder();
+
+        foreach (byte b in hash) sb.AppendFormat("{0:x2}", b);
+
+        return sb.ToString();
     }
+    
+    public string MD5(string input)
+    {
+        MD5 md5 = new MD5CryptoServiceProvider();
+        string pass = Console.ReadLine();
+        byte[] checkSum = md5.ComputeHash(Encoding.Unicode.GetBytes(input));
+        string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+
+        return result.ToLower();
+    }
+}
